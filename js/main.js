b9b0af1914b4563bbbadedc5f7f575000499953f
Dmytro Kovalenko <prestigio4040stud@gmail.com>
2021-10-01 00:38:54 +0200

    add

--- js/main.js
+++ js/main.js
@@ -14,3 +14,24 @@ $('.opinions .opinions__container').slick(
     adaptiveHeight: true,
   }
 );
+
+//doble slider with work
+$('.works__container').slick(
+  { 
+     slidesToShow: 1,
+    // slidesToScroll: 1,
+    // arrows: false,
+    // fade: true,
+    // infinite: true,
+    adaptiveHeight: true,
+    // asNavFor: '.photos__navigation'
+    }
+);
+// $('.photos__navigation').slick({
+//   slidesToShow: 3,
+//   slidesToScroll: 1,
+//   asNavFor: '.works__container',
+//   dots: true,
+//   centerMode: true,
+//   focusOnSelect: true
+// });
