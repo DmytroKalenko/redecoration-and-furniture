f762020be64d4e26866acad6c8052aaad404b511
Dmytro Kovalenko <prestigio4040stud@gmail.com>
2021-10-03 22:05:22 +0200

    add function scroll to links

--- js/main.js
+++ js/main.js
@@ -1,16 +1,15 @@
 $('.mainBanner').slick(
-    {
-        infinite: true,
-        dots: true,
-      }
+  {
+    infinite: true,
+    dots: true,
+  }
 );
 $('.opinions .opinions__container').slick(
   {
     infinite: true,
     dots: true,
     slidesToShow: 1,
-    slidesToScroll:1,
-    // centerMode: true,
+    slidesToScroll: 1,
     adaptiveHeight: true,
     initialSlide: 2,
   }
@@ -18,21 +17,39 @@ $('.opinions .opinions__container').slick(
 
 //doble slider with work
 $('.works__container').slick(
-  { 
- 
-     slidesToShow: 1,
+  {
+
+    slidesToShow: 1,
     slidesToScroll: 1,
     arrows: false,
     fade: true,
     infinite: true,
     adaptiveHeight: true,
     asNavFor: '.photos__navigation'
-    }
+  }
 );
 $('.photos__navigation').slick({
   slidesToShow: 5,
   slidesToScroll: 1,
   asNavFor: '.works__container',
-  // dots: true,
   focusOnSelect: true
 });
+
+
+/// scroll by 
+function moveTo() {
+  const links = ['--aboutUS', '--opinions', '--works', '--benefits', '--contact','footer--aboutUS','footer--opinions','footer--works', 'footer--contact', 'footer--benefits'];
+  links.forEach(element => {
+    let componentElement = element.slice(element.lastIndexOf('-')).replace('-','#')
+    document.querySelector(`.${element}`).addEventListener('click', () => {
+      $('html, body').animate({ scrollTop: $(`${componentElement}`).offset().top }, 1500);
+    })
+  });
+};
+moveTo();
+
+
+
+
+
+
