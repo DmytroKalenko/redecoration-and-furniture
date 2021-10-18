7a20b07d4ce2c8a2683bd9f2b3eb9ae8687c779c
Dmytro Kovalenko <prestigio4040stud@gmail.com>
2021-10-18 23:22:46 +0200

    Update sendMail.php

--- sendMail.php
+++ sendMail.php
@@ -9,10 +9,6 @@ $mail->CharSet = 'utf-8';
 
 $name = $_POST['name'];
 $phone = $_POST['phone'];
-
-
-//$mail->SMTPDebug = 3;                               // Enable verbose debug output
-
                                       // Set mailer to use SMTP
 $mail->Host = 'smtp.gmail.com';  																							// Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
@@ -22,13 +18,7 @@ $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, 
 $mail->Port = 465; // TCP port to connect to / этот порт может отличаться у других провайдеров
 
 $mail->setFrom('meancook.dy@gmail.com'); // от кого будет уходить письмо?
-$mail->addAddress('mivetig959@wawue.com');     // Кому будет уходить письмо 
-//$mail->addAddress('ellen@example.com');               // Name is optional
-//$mail->addReplyTo('info@example.com', 'Information');
-//$mail->addCC('cc@example.com');
-//$mail->addBCC('bcc@example.com');
-//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
-//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+$mail->addAddress('prestigio4040stud@gmail.com');     // Кому будет уходить письмо 
 $mail->isHTML(true);                                  // Set email format to HTML
 
 $mail->Subject = 'Заявка с тестового сайта';
