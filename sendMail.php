e4eac856d30b68f26d499c67be24b2a80ccf76f5
Dmytro Kovalenko <prestigio4040stud@gmail.com>
2021-10-18 23:20:43 +0200

    added phpmailer

---
+++ sendMail.php
@@ -0,0 +1,58 @@
+<?php 
+
+require_once('phpmailer/PHPMailerAutoload.php');
+
+$mail = new PHPMailer(true);
+$mail->isSMTP();
+$mail->SMTPDebug = SMTP::DEBUG_SERVER;
+$mail->CharSet = 'utf-8';
+
+$name = $_POST['name'];
+$phone = $_POST['phone'];
+
+
+//$mail->SMTPDebug = 3;                               // Enable verbose debug output
+
+                                      // Set mailer to use SMTP
+$mail->Host = 'smtp.gmail.com';  																							// Specify main and backup SMTP servers
+$mail->SMTPAuth = true;                               // Enable SMTP authentication
+$mail->Username = 'meancook.dy@gmail.com'; // Ваш логин от почты с которой будут отправляться письма
+$mail->Password = 'Malma2033'; // Ваш пароль от почты с которой будут отправляться письма
+$mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
+$mail->Port = 465; // TCP port to connect to / этот порт может отличаться у других провайдеров
+
+$mail->setFrom('meancook.dy@gmail.com'); // от кого будет уходить письмо?
+$mail->addAddress('mivetig959@wawue.com');     // Кому будет уходить письмо 
+//$mail->addAddress('ellen@example.com');               // Name is optional
+//$mail->addReplyTo('info@example.com', 'Information');
+//$mail->addCC('cc@example.com');
+//$mail->addBCC('bcc@example.com');
+//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+$mail->isHTML(true);                                  // Set email format to HTML
+
+$mail->Subject = 'Заявка с тестового сайта';
+$mail->Body    = '' .$name . ' оставил заявку, его телефон ' .$phone;
+$mail->AltBody = '';
+
+
+
+
+// if (!$mail->send()) {
+//     echo 'Mailer Error: ' . $mail->ErrorInfo;
+// } else {
+//     echo 'Message sent!';
+//     //Section 2: IMAP
+//     //Uncomment these to save your message in the 'Sent Mail' folder.
+//     #if (save_mail($mail)) {
+//     #    echo "Message saved!";
+//     #}
+// }
+
+
+if(!$mail->send()) {
+    echo 'Error';
+} else {
+    header('location: thank-you.html');
+}
+?>
